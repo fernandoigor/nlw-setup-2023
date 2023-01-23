2ddb7cf5e1edb2d51f80fc91d5d32b5f2d17f4bc
Fernando <fpinheiro@nwsoft.com.br>
2023-01-23 17:34:54 -0300

    feat: Aplicar loading padrão na aplicação

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -1,16 +1,18 @@
-import { ActivityIndicator, View } from "react-native";
-
+import { View, Text, Dimensions } from "react-native";
+import { LoadingHabits } from "./LoadingHabits";
+const MARGIN_LEFT = (Dimensions.get("screen").width - 32 - 32 * 6) / 4;
 export function Loading() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#09090A",
-      }}
-    >
-      <ActivityIndicator color="#7C3AED" size={"large"} />
+    <View className="flex-1 items-start justify-center bg-background px-8 pt-16">
+      <View
+        style={{
+          marginLeft: MARGIN_LEFT,
+        }}
+      >
+        <LoadingHabits />
+      </View>
+
+      <Text className="mt-2 mx-auto text-white">Loading...</Text>
     </View>
   );
 }
